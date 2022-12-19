24cadb4a2cc0bad9886d422cb3ea5b48e984b06c
chang-seop <ikavon@naver.com>
2022-12-19 19:33:13 +0900

    ADD 행렬의 곱셈
    Programmers Level 2

---
+++ src/Level2/P12949/Main.java
@@ -0,0 +1,30 @@
+package Level2.P12949;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] arr1 = {{1, 4}, {3, 2}, {4, 1}};
+        int[][] arr2 = {{3, 3}, {3, 3}};
+        Solution solution = new Solution();
+        int[][] result = solution.solution(arr1, arr2);
+        for(int[] i : result) {
+            for(int j : i) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2[0].length; j++) {
+                for (int k = 0; k < arr1[0].length; k++) {
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+        return answer;
+    }
+}
