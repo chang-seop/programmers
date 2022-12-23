ad8797b8ac48275b6f9aeb81d9b6b2f68785c7c9
chang-seop <ikavon@naver.com>
2022-12-24 08:33:24 +0900

    ADD 약수의 합
    Programmers Level 1

--- out/production/untitled/Level1/P12944/README.md
+++ out/production/untitled/Level1/P12944/README.md
@@ -1,2 +1,2 @@
 # [평균 구하기](https://school.programmers.co.kr/learn/courses/30/lessons/12944)
-배열
+수학 구현
---
+++ src/Level1/P12928/Main.java
@@ -0,0 +1,20 @@
+package Level1.P12928;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 12;
+        Solution solution = new Solution();
+        int result = solution.solution(n);
+        System.out.println("result = " + result);
+    }
+}
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            if(n % i == 0) answer += i;
+        }
+        return answer;
+    }
+}
